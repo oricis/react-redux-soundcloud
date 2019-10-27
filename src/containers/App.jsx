1c16dc328d735becf028b6a45a6f665351c2d127
oricis <m_alc1@hotmail.com>
2019-10-27 19:38:34 +0100

    created main component and migrated styles

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import '../assets/styles/reset.css';
+import '../assets/styles/base.scss';
+import '../assets/styles/App.scss';
+
+class App extends Component
+{
+
+    render()
+    {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default App;
