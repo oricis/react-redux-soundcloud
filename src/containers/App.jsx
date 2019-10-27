3c4cf001259647448ef2a5e3892c08d8baa93528
oricis <m_alc1@hotmail.com>
2019-10-27 19:46:03 +0100

    moved HTML from "index.html" to "App / render()"

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -10,7 +10,37 @@ class App extends Component
     render()
     {
         return (
-            <div></div>
+            <div>
+                <article>
+                    <h3 className="color-electricblue">Busca tu canción...</h3>
+
+                    <div className="form">
+                        <form name="search-form" id="search-form">
+                            <input type="text" name="search" id="search" />
+
+                            <input type="submit" className="btn left-side-btn" id="search-button" value="Buscar" />
+                        </form>
+                    </div>
+
+                    /*** Search results (start dragging) ***/
+                    <div className="results"></div>
+                </article>
+
+                <article className="hidden" id="playing-song">
+                    <h3 className="color-electricblue">Canción actual</h3>
+
+                    /*** Song playing (end dragging) ***/
+                    <div className="actual-song"
+                        ondragover="allowDrop(event)"
+                        ondrop="dragEnd(event)"
+                        id="actual-song">
+
+                        <span className="droppable-zone-info">
+                            Arrastra aquí para reproducir ...
+                        </span>
+                    </div>
+                </article>
+            </div>
         );
     }
 }
